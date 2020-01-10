d5c4f61e00e92f3f1d3a685840df6ff0238ec760
RQH <656418510@qq.com>
2020-01-11 06:50:41 +0800

    Update config code

--- index.js
+++ index.js
@@ -1,74 +1,76 @@
 'use strict';
 
-var files = [];
 var fs = require('fs');
-var config = hexo.config;
 var path = require('path');
 var log = require('hexo-log')({name: 'hexo-site-auth', debug: false});
 
+var files = [];
+var config = hexo.config;
+var site_auth = config.site_auth;
+
+if (!site_auth) {
+  return;
+}
+
 // WeiXin GongZhongHao
-var weixin_path =  config.site_auth.weixin_path;
-var weixin_content = config.site_auth.weixin_content;
+var weixin_path =  site_auth.weixin_path;
+var weixin_content = site_auth.weixin_content;
 
 // BaiDu ZhanZhang
-var baidu_path = config.site_auth.baidu_path;
-var baidu_content = config.site_auth.baidu_content;
+var baidu_path = site_auth.baidu_path;
+var baidu_content = site_auth.baidu_content;
 
 // Google ZhanZhang
-var google_path = config.site_auth.google_path;
-var google_content = config.site_auth.google_content;
+var google_path = site_auth.google_path;
+var google_content = site_auth.google_content;
 
 // Google Adsense
-var google_adsense_path = config.site_auth.google_adsense_path;
-var google_adsense_content = config.site_auth.google_adsense_content;
+var google_adsense_path = site_auth.google_adsense_path;
+var google_adsense_content = site_auth.google_adsense_content;
 
 // SoGou ZhanZhang
-var sogou_path = config.site_auth.sogou_path;
-var sogou_content = config.site_auth.sogou_content;
+var sogou_path = site_auth.sogou_path;
+var sogou_content = site_auth.sogou_content;
 
-if (config.site_auth) {
+if(weixin_path && weixin_content){
+  files.push({
+    path: weixin_path,
+    data: weixin_content
+  });
+}
 
-  if(weixin_path && weixin_content){
-    files.push({
-      path: weixin_path,
-      data: weixin_content
-    });
-  }
+if (baidu_path && baidu_content) {
+  files.push({
+    path: baidu_path,
+    data: baidu_content
+  });
+}
 
-  if (baidu_path && baidu_content) {
-    files.push({
-      path: baidu_path,
-      data: baidu_content
-    });
-  }
+if (google_path && google_content) {
+  files.push({
+    path: google_path,
+    data: google_content
+  });
+}
 
-  if (google_path && google_content) {
-    files.push({
-      path: google_path,
-      data: google_content
-    });
-  }
+if (google_adsense_path && google_adsense_content) {
+  files.push({
+    path: google_adsense_path,
+    data: google_adsense_content
+  });
+}
 
-  if (google_adsense_path && google_adsense_content) {
-    files.push({
-      path: google_adsense_path,
-      data: google_adsense_content
-    });
-  }
+if(sogou_path && sogou_content){
+  files.push({
+    path: sogou_path,
+    data: sogou_content
+  });
+}
 
-  if(sogou_path && sogou_content){
-    files.push({
-      path: sogou_path,
-      data: sogou_content
-    });
-  }
+if (files.length > 0) {
+  log.info('generate site auth file');
 
-  if (files.length > 0) {
-    log.info('generate site auth file');
-
-    hexo.extend.generator.register('site-auth-generator', function(locals) {
-      return files;
-    });
-  }
-
+  hexo.extend.generator.register('site-auth-generator', function(locals) {
+    return files;
+  });
 }
